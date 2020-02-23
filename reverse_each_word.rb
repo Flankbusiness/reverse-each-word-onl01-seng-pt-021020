c8550a9cb0273ee82c6ad32b4ef9391e71964ef8
Steven Prindle <flankbusiness@gmail.com>
2020-02-23 02:54:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
-sentence1 = "Hello there, and how are you?"
+sentence1 = ["Hello there, and how are you?"]
 def reverse_each_word(sentence1)
-  sentence1.map do |sentence|
+  sentence1.collect do |sentence|
     binding.pry
     puts sentence1
   end
