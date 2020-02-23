cc4a44432d81c066b4fe4076c43cc3935f353eb4
Steven Prindle <flankbusiness@gmail.com>
2020-02-23 02:40:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,18 @@
 def reverse_each_word()
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ # def hamburger(toppings)
+  my_statements = []
+  toppings.each do |topping|
+    my_statements << "I love #{topping} on my burgers!"
+  end
+  my_statements
+end
